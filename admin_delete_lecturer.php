16c6751d2107dbf97b1061040d2611f4a9ac162f
ziyanoffl <ziyan.offl@gmail.com>
2023-01-01 22:26:51 +0530

    Testing pull request 2 testing pull request 2

--- admin_delete_lecturer.php
+++ admin_delete_lecturer.php
@@ -8,6 +8,7 @@ $sql = "DELETE FROM lecturer_reg WHERE lecturer_id = $Id";
 
 $status = mysqli_query($con,$sql);
 
+
 if(!isset($_SESSION['admin_username'])){
     echo "<script>
         alert('Please login to view this page!');
